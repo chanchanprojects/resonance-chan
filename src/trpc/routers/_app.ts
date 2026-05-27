2ddcf4b720d99b5a896f0cb218cc22451563beb6
Chan <137618152+chanchan012@users.noreply.github.com>
2026-05-27 12:43:10 +0800

    04: backend infrastructure

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+
+export const appRouter = createTRPCRouter({
+    
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
